e45aa4a629eddda5e0d82e718848797caedb86f6
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2022-12-08 10:16:29 -0500

    7

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,12 +2,22 @@ import "../styles/style.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
+
+// D;
 AOS.init();
 import { menu } from "./menu";
 document.querySelector("#app").innerHTML = `
-  <h1>Hello!</h1>
+  <h1>Angela's restaurant</h1>
   <h2>Hi!</h2>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
-
 console.log(menu);
+
+/* const product = {
+
+.filter((food) => food.type.includes("Main course"))
+.forEach((food) => {
+  console.log(food.name);
+});
+}
+ */
