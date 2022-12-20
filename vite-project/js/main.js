b7eea7eddffa12f1693056135996a15d1a5fc7fb
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2022-12-20 10:16:35 -0500

    17

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -13,30 +13,56 @@ const DOMSelectors = {
   displaySection: document.getElementById("display"),
 };
 
+/* 
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1> 
+      <p>Architect & Engineer</p> 
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+ */
+
 function card() {
   menu.forEach((food) => {
     DOMSelectors.displaySection.insertAdjacentHTML(
       "beforeend",
-      `<div class="display-card" data-aos="flip-up">
-  <h3 class= "display-name"> ${food.name} </h3>
-  <h4 class= "display-type"> ${food.type} </h4>
-  <img class= "image" src= "${food.img}"/> 
-  </div>`
+      `<div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+        <h3 class="display-name"> ${food.name} </h3>
+        <h4 class="display-type"> ${food.type} </h4>
+        <img class="image" src="${food.img}" />
+        </div>
+        <div class="flip-card-back">
+        <h3 class="display-vegetarian"> Vegetarian: ${food.vegetarian} </h3>
+        <h3 class="display-price"> Price: ${food.price} $ </h3>
+        <h3 class="display-stock"> In Stock: ${food.instock} </h3>
+        </div>
+
+        
+      </div>
+    </div>`
     );
   });
 }
 card();
 
 {
-  /* <h3 class= "display-vegetarian"> Vegetarian: ${food.vegetarian} </h3>
-  <h3 class= "display-price"> Price: ${food.price} $ </h3>
-  <h3 class= "display-stock"> In Stock: ${food.instock} </h3> */
+  /* <h3 class="display-name"> ${food.name} </h3>
+              <h4 class="display-type"> ${food.type} </h4>
+              <img class="image" src="${food.img}" />*/
 }
 
 /* <h3 class="display-vegetarian"> Vegetarian: ${food.vegetarian} </h3>
     <h3 class="display-price"> Price: ${food.price} $ </h3>
     <h3 class="display-stock"> In Stock: ${food.instock} </h3>
-    </div>` */
+    */
 
 /* const product = {
 .filter((food) => food.type.includes("Main course"))
